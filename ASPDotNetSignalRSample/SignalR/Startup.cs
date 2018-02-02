ae8a9e44ed457bcea71e7e3fb16b382953e359a3
chuangmaster <masterchuang@urtody.com>
2018-02-02 18:02:54 +0800

    因應跨站調整

--- ASPDotNetSignalRSample/SignalR/Startup.cs
+++ ASPDotNetSignalRSample/SignalR/Startup.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Owin;
 using Owin;
 using ASPDotNetSignalRSample;
+//允許CORS
+using Microsoft.Owin.Cors;
+using Microsoft.AspNet.SignalR;
+
 [assembly: OwinStartup(typeof(ASPDotNetSignalRSample.SignalR.Startup))]
 namespace ASPDotNetSignalRSample.SignalR
 {
@@ -8,7 +12,18 @@ namespace ASPDotNetSignalRSample.SignalR
     {
         public void Configuration(IAppBuilder app)
         {
-            app.MapSignalR();
+            //不允許跨站
+            //app.MapSignalR();
+            //允許跨站
+            app.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguraion = new HubConfiguration
+                {
+                    EnableDetailedErrors = true
+                };
+                map.RunSignalR(hubConfiguraion);
+            });
         }
     }
 }
