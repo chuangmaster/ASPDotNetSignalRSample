4d7b952d622b72190fe2aa812acfb80c8c606310
chuangmaster <masterchuang@urtody.com>
2018-02-02 10:27:53 +0800

    註冊Startup

---
+++ ASPDotNetSignalRSample/SignalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+using ASPDotNetSignalRSample;
+[assembly: OwinStartup(typeof(ASPDotNetSignalRSample.SignalR.Startup))]
+namespace ASPDotNetSignalRSample.SignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
