9a9e38e2abf32b0418d913cefb71d6acf51a1d6b
chuangmaster <masterchuang@urtody.com>
2018-02-02 10:27:23 +0800

    建立Hub

---
+++ ASPDotNetSignalRSample/SignalR/BroadCastHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ASPDotNetSignalRSample.SignalR
+{
+    public class BroadcastHub : Hub
+    {
+        //允許Client端呼叫的方法
+        public void SendMessage(string name, string message)
+        {
+            //Server端呼叫所有客戶端
+            Clients.All.SendMessage(name, message);
+        }
+    }
+}
