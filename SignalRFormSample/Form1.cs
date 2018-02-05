7cd14186bebe24be301de6ba366b2690aa7d1a5c
chuangmaster <masterchuang@urtody.com>
2018-02-05 11:49:38 +0800

    實作SignalR Client

--- SignalRFormSample/Form1.cs
+++ SignalRFormSample/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,19 +13,36 @@ namespace SignalRFormSample
 {
     public partial class Form1 : Form
     {
+        //建立連線
+        HubConnection hubConnection = new HubConnection("http://localhost:55500/");
+        IHubProxy proxy;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
+        delegate void InvokeMethod(string name, string message);
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            // 建立 Hub Proxy
+            proxy = hubConnection.CreateHubProxy("broadcastHub");
+            // 建立 Invoke內容
+            InvokeMethod invoker = (name, message) => content.AppendText($"{name}:{message}\n");
+            //註冊給伺服端呼叫的方法
+            proxy.On<string, string>("SendMessage", (name, message) =>
+            content.Invoke(invoker, name, message)
+            );
+            //連線 SignalR Server
+            hubConnection.Start().Wait();
         }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //執行 SinalR Server Method
+            await proxy.Invoke("SendMessage", UserName.Text, Message.Text);
+        }
+
+
     }
 }
