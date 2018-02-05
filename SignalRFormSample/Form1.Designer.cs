db9c684bc4da2f31adbd828307d648fe25a74e16
chuangmaster <masterchuang@urtody.com>
2018-02-05 10:51:16 +0800

    加入Form專案 與 UI基本配置

---
+++ SignalRFormSample/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SignalRFormSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Message = new System.Windows.Forms.TextBox();
+            this.content = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // UserName
+            // 
+            this.UserName.Location = new System.Drawing.Point(64, 23);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(100, 22);
+            this.UserName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message";
+            // 
+            // Message
+            // 
+            this.Message.Location = new System.Drawing.Point(64, 55);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(100, 22);
+            this.Message.TabIndex = 2;
+            // 
+            // content
+            // 
+            this.content.Location = new System.Drawing.Point(12, 92);
+            this.content.Multiline = true;
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(260, 128);
+            this.content.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.content);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.UserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Message;
+        private System.Windows.Forms.TextBox content;
+    }
+}
+
