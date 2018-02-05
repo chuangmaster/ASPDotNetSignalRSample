7098d1157376395dd151be233e7a876c89ee1b62
chuangmaster <masterchuang@urtody.com>
2018-02-05 10:38:05 +0800

    console 使用 SignalR client

--- SignalRConsoleSample/Program.cs
+++ SignalRConsoleSample/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,23 @@ namespace SignalRConsoleSample
     {
         static void Main(string[] args)
         {
+            RunProxy();
+            Console.Read();
+        }
+        public static async void RunProxy()
+        {
+            //建立連線
+            var hubConnection = new HubConnection("http://localhost:55500/");
+            //建立 Hub Proxy
+            IHubProxy proxy = hubConnection.CreateHubProxy("broadcastHub");
+            //註冊給伺服器端呼叫的方法
+            proxy.On<string, string>("SendMessage", (name, message) => Console.WriteLine($"{name}:{message}"));
+
+            //連線到 SignalR Server
+            await hubConnection.Start();
+
+            //執行Signal Server 方法
+            await proxy.Invoke("SendMessage", "console", "Hi");
         }
     }
 }
