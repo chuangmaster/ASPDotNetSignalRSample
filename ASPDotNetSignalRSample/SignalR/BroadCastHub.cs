392ba91c788d7fb8f01edf128a4470205a2cfc24
chuangmaster <chuang.master@gmail.com>
2018-02-12 00:38:45 +0800

    使用非同步來進行訊息傳遞

--- ASPDotNetSignalRSample/SignalR/BroadCastHub.cs
+++ ASPDotNetSignalRSample/SignalR/BroadCastHub.cs
@@ -14,6 +14,7 @@ namespace ASPDotNetSignalRSample.SignalR
         //允許Client端呼叫的方法
         public void SendMessage(string name, string message)
         {
+            //取得自訂的屬性
             var userName = Clients.Caller.UserName;
             connectionList.Add(Context.ConnectionId);
             //Server端呼叫所有客戶端
@@ -24,9 +25,15 @@ namespace ASPDotNetSignalRSample.SignalR
 
             //使用ClientProxy來做訊息發送
             IClientProxy clientProxy = Clients.All;
-            clientProxy.Invoke("SendMessage", new { name = name, message = message });
+            clientProxy.Invoke("SendMessage", name, message);
         }
 
+        public async Task SendAlert(string name)
+        {
+            IClientProxy clientProxy = Clients.All;
+            await clientProxy.Invoke("SendAlert", new { name = name });
+            Clients.All.Invoke("SendMessage", "系統", $"{name}上線了");
+        }
         /// <summary>
         /// 生命週期 - On Connected
         /// </summary>
